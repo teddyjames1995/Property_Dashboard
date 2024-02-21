dbe76c872dd48582e4120e619c61d3ad1913b35d
teddyjames1995 <teddyjames1995@hotmail.com>
2024-02-21 00:13:29 +0000

    Create Admin Interface

--- Property_Dashboard/properties/admin.py
+++ Property_Dashboard/properties/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Property, Tenant, OperatingExpenses
 
-# Register your models here.
+admin.site.register(Property)
+admin.site.register(Tenant)
+admin.site.register(OperatingExpenses)
