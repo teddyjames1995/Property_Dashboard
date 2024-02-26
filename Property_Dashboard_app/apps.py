0b5ac116f4faecbeba086cf92c91917af6a9ac4c
teddyjames1995 <teddyjames1995@hotmail.com>
2024-02-26 00:19:08 +0000

    Update Property_Dashboard_app name in apps.py

--- Property_Dashboard_app/apps.py
+++ Property_Dashboard_app/apps.py
@@ -1,6 +1,4 @@
 from django.apps import AppConfig
 
-
-class PropertiesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'properties'
+class PropertyDashboardAppConfig(AppConfig):
+    name = 'Property_Dashboard_app'
