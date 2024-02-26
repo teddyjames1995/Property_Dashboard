70197e5eeaa75149412ea57aae9ee73ea8183592
teddyjames1995 <teddyjames1995@hotmail.com>
2024-02-26 00:30:16 +0000

    Modified Views.py

--- Property_Dashboard_app/views.py
+++ Property_Dashboard_app/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
-def home(request):
+def home_view(request):
     return HttpResponse("Welcome to the Property Dashboard!")
