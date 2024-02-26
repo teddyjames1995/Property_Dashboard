2f410b4a6218a2888731f0d351f5f79b421f9761
teddyjames1995 <teddyjames1995@hotmail.com>
2024-02-26 18:15:24 +0000

    Change views

--- Property_Dashboard_app/views.py
+++ Property_Dashboard_app/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_view(request):
-    return HttpResponse("Welcome to the Property Dashboard!")
+    # Your view logic here
+    return render(request, 'home.html', {})
