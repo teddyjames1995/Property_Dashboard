7531042da834bfe7fc18a4dc130aa2eca8209010
teddyjames1995 <teddyjames1995@hotmail.com>
2024-03-16 19:33:41 +0000

    CSS and JavaScript Linking

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -2,6 +2,26 @@
 function renderSectorAllocationChart(data) {
     // Chart.js function to render pie chart
 }
+// Dummy function to simulate data fetching based on dropdowns
+// In a real app, you might fetch data from the server
+function fetchDataForCharts() {
+    // Simulated data update
+    updateSectorChart(['Industrial', 'Offices'], [30, 70], ['#FF6384', '#36A2EB']);
+    updateLeaseExpiryChart(['2024', '2025', '2026'], [20, 30, 25]);
+}
+
+function updateSectorChart(labels, data, backgroundColors) {
+    sectorChart.data.labels = labels;
+    sectorChart.data.datasets[0].data = data;
+    sectorChart.data.datasets[0].backgroundColor = backgroundColors;
+    sectorChart.update();
+}
+
+function updateLeaseExpiryChart(labels, data) {
+    leaseExpiryChart.data.labels = labels;
+    leaseExpiryChart.data.datasets[0].data = data;
+    leaseExpiryChart.update();
+}
 
 function updateChartsOnDropdownChange() {
     // Function to update charts when dropdown values change
